64dda6dd0af2bf964b0dc4469d739fd2eb588371
y2kim <kchy0806@naver.com>
2017-10-07 19:00:21 +0900

    1번째 작업
    http://www.java2s.com/Open-Source/Android_Free_Code/Sudoku/Download_Free_code_sudoku_android_tutorial.htm    참조함

--- Games/app/src/main/java/com/androidgame/kcy/games/MainActivity.java
+++ Games/app/src/main/java/com/androidgame/kcy/games/MainActivity.java
@@ -2,12 +2,34 @@ package com.androidgame.kcy.games;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Sudoku"; // 스도쿠라는 이름은 항상 변하지 않음
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button continueButton =(Button)findViewById(R.id.continue_button);
+
+        Button newButton = (Button) findViewById(R.id.new_button);
+
+        Button aboutButton =(Button) findViewById(R.id.about_button);
+
+        Button exitButton =(Button) findViewById(R.id.exit_button);
+
     }
 }
